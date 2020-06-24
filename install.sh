19d77fefab60fe18f0a066f3871cb7007789af13
Orlando Brea <orlando.brea@parserdigital.com>
2020-06-24 13:55:48 -0300

    Corrijo ruta de install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 LB_FOLDER="load_balancer"
 
-git clone https://github.com/andes-k8s/dcm4che-load-balancer.git /git/$LB_FOLDER
-git clone https://github.com/andes-k8s/dcm4che-cert-updater.git /git/cert_updater
+git clone https://github.com/andes-k8s/dcm4che-load-balancer.git $LB_FOLDER
+git clone https://github.com/andes-k8s/dcm4che-cert-updater.git cert_updater
 
 cp cert_updater/*.sh $LB_FOLDER/
 cp -R cert_updater/template $LB_FOLDER/
